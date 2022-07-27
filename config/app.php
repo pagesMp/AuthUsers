fa6985b1a0b724dc9f4093b5b2985318ea02afb8
pagesMp <marc.msp97@gmail.com>
2022-07-27 13:06:11 +0200

    feat: tymon jwt configure

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //JWT tymo
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*

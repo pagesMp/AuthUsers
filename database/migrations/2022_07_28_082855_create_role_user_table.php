26b9208f48afd0103baedce19fab8b4192b31806
pagesMp <marc.msp97@gmail.com>
2022-07-28 11:21:50 +0200

    feat:poner rolunicos

--- database/migrations/2022_07_28_082855_create_role_user_table.php
+++ database/migrations/2022_07_28_082855_create_role_user_table.php
@@ -19,6 +19,7 @@ class CreateRoleUserTable extends Migration
             $table->unsignedBigInteger('role_id');
             $table->timestamps();
 
+            $table->unique(['user_id', 'role_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
